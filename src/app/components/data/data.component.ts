1535d4afa5a58b6fe1c483b3ff6981a03d3667f7
codethealbert <ing.alberto.lopez.l27@gmail.com>
2019-02-22 18:38:31 -0600

    Data: Reset y carga de la data al formulario a partir de un objeto

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,7 +11,7 @@ export class DataComponent {
 
   forma: FormGroup;
 
-  usuario: Object = {
+  usuario = {
     nombrecompleto: {
       nombre: 'Alberto',
       apellido: 'Lopez'
@@ -29,11 +29,21 @@ export class DataComponent {
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
 
     });
+    this.forma.setValue(this.usuario);
   }
 
   guardarCambios() {
     console.log(this.forma.value);
     console.log(this.forma);
+
+    // this.forma.reset(this.usuario);
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: '',
+        apellido: ''
+      },
+      correo: ''
+    });
   }
 
 }
